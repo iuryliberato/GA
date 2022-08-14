c68ea8f1a0792a3c16652fcb41b13496fa4a79a7
iuryliberato <iuryliberato@hotmail.com>
2022-08-14 19:06:35 +0100

    todo list

---
+++ W2D2/functions.js
@@ -0,0 +1,15 @@
+
+// let number
+// const diceRoll = (number) => {
+//   console.log(`${number}`)
+// }
+// diceRoll(2)
+// diceRoll(Math.random(number))
+
+
+// const rollDice = (number) => {
+//   const doubleDie = number*2;
+//   return doubleDie;
+// }
+
+// console.log(rollDice(Math.floor(1 + Math.random() * 6)));
