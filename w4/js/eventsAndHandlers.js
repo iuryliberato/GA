a2560a81607dd40f9aa4e9c81d89dc072059c4a3
iuryliberato <iuryliberato@hotmail.com>
2022-08-15 19:39:20 +0100

    new commit

---
+++ w4/js/eventsAndHandlers.js
@@ -0,0 +1,10 @@
+// function init() {
+//   const button = document.querySelectorAll('.buttons')
+
+//   function onClick() {
+//     console.log('click me')
+//   }
+//   button.forEach(buttons => buttons.addEventListener('click', onClick))
+// }
+
+// window.addEventListener('DOMContentLoaded', init)
